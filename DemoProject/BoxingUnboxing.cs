d23924b1c015706bc4bb2b251097194b380d3e2a
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-08 07:13:09 +0530

    Boxing Unboxing Prog added

---
+++ DemoProject/BoxingUnboxing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class BoxingUnboxing
+    {
+        static void Main()
+        {
+            int x = 5;
+            int y = 3;
+            int w;
+            Object o;
+            o = x;
+            o = 10;
+            w=y* (int)o;
+            //w = y;
+            //w = (int)o;
+            Console.WriteLine(w);
+            Console.WriteLine(x);
+            Console.WriteLine(o);
+        }
+    }
+}
