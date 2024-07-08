efdd7a251ac4af4f599b5647385829a9e201ead1
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-08 07:19:16 +0530

    Pre Post Increment prog added

---
+++ DemoProject/PrePostIncrement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class PrePostIncrement
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = a++;
+            Console.WriteLine(b);
+            int c = ++a;
+            Console.WriteLine(c);
+        }
+    }
+}
