77db47a28de72676b5bbf4bb7d509eb2d8300ed5
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-08 07:18:22 +0530

    Function return value using out keyword throught parameter

---
+++ DemoProject/MethodOut.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class abc
+    {
+        public void sum(int a, int b, out int c)
+        {
+            c = a + b;
+        }
+    }
+
+    internal class MethodOut
+    {
+        static void Main()
+        {
+            int a;
+            abc l = new abc();
+            l.sum(20, 25,out a);
+            Console.WriteLine(a);
+        }
+        
+    }
+}
