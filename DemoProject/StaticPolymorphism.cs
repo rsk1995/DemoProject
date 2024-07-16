2f9d5a3f4ce2c325cff938e31348f31abec6da6c
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-16 10:08:08 +0530

    Static Polymorphism concept implemented using area of circle, rectangle and triangle

---
+++ DemoProject/StaticPolymorphism.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class Calculator
+    {
+        const double pi= 3.14;
+        public void Area(double radius)
+        {
+            double ar = pi * radius * radius;
+            Console.WriteLine("Area of circle: "+ar);
+        }
+        public void Area(double length, double width)
+        {
+            double ar = length * width;
+            Console.WriteLine("Area of Rectangle: "+ar);
+            ar = (length * width)/2;
+            Console.WriteLine("Area of Triangle: " + ar);
+        }
+    }
+
+    class StaticPolymorphism
+    {
+        static void Main()
+        {
+            Calculator cal = new Calculator();
+            cal.Area(7.5);
+            cal.Area(5.4, 6.8);
+        }
+    }
+}
