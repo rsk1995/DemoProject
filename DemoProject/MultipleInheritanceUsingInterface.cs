3cb00388ebd117dabbaee2fd191194acf0594223
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-18 12:04:41 +0530

    Interface Concept Program

---
+++ DemoProject/MultipleInheritanceUsingInterface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    interface IPlayable
+    {
+        void Play();
+    }
+    class VideoPlayer : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing video...");
+            Console.WriteLine();
+        }
+    }
+    class MusicPlayer : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing music...");
+        }
+    }
+    class MultipleInheritanceUsingInterface
+    {
+        static void Main()
+        {
+            VideoPlayer VP = new VideoPlayer();
+            VP.Play();
+            MusicPlayer MP = new MusicPlayer();
+            MP.Play();
+        }
+    }
+}
