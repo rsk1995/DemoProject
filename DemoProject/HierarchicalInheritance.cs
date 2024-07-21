b2d7cddd994744b8f310b7d64ec59d11880b7a96
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-21 19:31:50 +0530

    Hierarchical Inheritance program

---
+++ DemoProject/HierarchicalInheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    abstract class Shape1
+    {
+        public abstract void DisplayDetails();
+    }
+    class Circle1 : Shape1
+    {
+        public override void DisplayDetails()
+        {
+            Console.WriteLine("Shape: Circle");
+            Console.WriteLine("Color: Red");
+            Console.WriteLine();
+        }
+    }
+    class Rectangle1 : Shape1
+    {
+        public override void DisplayDetails()
+        {
+            Console.WriteLine("Shape: Rectangle");
+            Console.WriteLine("Color: Yellow");
+            Console.WriteLine();
+        }
+    }
+
+    internal class HierarchicalInheritance
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hierarchical Inheritance...!");
+            Console.WriteLine();
+            Shape1 a = new Circle1();
+            a.DisplayDetails();
+            Shape1 b = new Rectangle1();
+            b.DisplayDetails();
+        }
+    }
+}
