3a4934ef8e9533a8e76daffb32eea6f6afc7834d
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-25 11:20:29 +0530

    Generic Collection type 1 List concept program

---
+++ DemoProject/ListGenericCollections.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class ListGenericCollections
+    {
+        static void Main()
+        {
+            List<string> lst = new List<string>();
+            lst.Add("RAMESH");
+            lst.Add("SARVESH");
+            lst.Add("KIRAN");
+            lst.Add("AMOL");
+
+            Console.WriteLine("List elements are: ");
+            foreach (string item in lst)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            
+            lst.Add("RAJ");
+            lst.Add("MAYUR");
+
+            Console.WriteLine("Updated list elements are: ");
+            foreach (string item in lst)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            lst.RemoveAt(0);
+
+            Console.WriteLine("Updated list (after deleting) elements are: ");
+            foreach (string item in lst)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Does list contain 'RAJ'? : " + lst.Contains("RAJ"));
+
+        }
+    }
+}
