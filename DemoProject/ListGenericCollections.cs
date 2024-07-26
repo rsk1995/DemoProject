6620847632c480bebdcc0afb52b49db54a478081
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-07-26 09:40:24 +0530

    Generic Collection type List concept program changes per assignment instructions

--- DemoProject/ListGenericCollections.cs
+++ DemoProject/ListGenericCollections.cs
@@ -22,9 +22,9 @@ namespace DemoProject
                 Console.WriteLine(item);
             }
             Console.WriteLine();
-            
-            lst.Add("RAJ");
-            lst.Add("MAYUR");
+
+            lst.Insert(1, "RAJ");
+            lst.Insert(4, "MAYUR");
 
             Console.WriteLine("Updated list elements are: ");
             foreach (string item in lst)
@@ -33,7 +33,7 @@ namespace DemoProject
             }
             Console.WriteLine();
 
-            lst.RemoveAt(0);
+            lst.RemoveAt(0);          
 
             Console.WriteLine("Updated list (after deleting) elements are: ");
             foreach (string item in lst)
@@ -43,7 +43,10 @@ namespace DemoProject
             Console.WriteLine();
 
             Console.WriteLine("Does list contain 'RAJ'? : " + lst.Contains("RAJ"));
-
+            Console.WriteLine("How many elements in list? : " + lst.Count);
+            lst.Clear();
+            Console.WriteLine("List cleared!");
+            Console.WriteLine("How many elements in list? : " + lst.Count);
         }
     }
 }
