15f71f65e3f426cedf378964e610cca667d907e3
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-08-01 09:12:20 +0530

    Ref type concept program

---
+++ DemoProject/RefExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class RefExample
+    {
+        static void Main()
+        {
+            int a = 10;
+            IncrementValue(ref a);
+            Console.WriteLine("Value passed as 10 & incremented value is: " + a);
+        }
+        static void IncrementValue(ref int b)
+        {
+            b++;
+        }
+    }
+}
