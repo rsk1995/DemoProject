b739f1b8230daee6ba062c1477081dadf39de970
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-08-01 09:26:13 +0530

    Out concept program

---
+++ DemoProject/OutExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    internal class OutExample
+    {
+        static void Main()
+        {
+            InitializeValues(out int x, out string y);
+            Console.WriteLine("Entered value is: " + x);
+            Console.WriteLine("Entered string is: " + y);
+        }
+        static void InitializeValues(out int a, out string b)
+        {
+            Console.Write("Enter any value: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter any string: ");
+            b = Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
