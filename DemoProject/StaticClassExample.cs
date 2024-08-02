4d7c860abc64dd0c208a62db5c636a6ab89e5cb2
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-08-02 09:33:42 +0530

    Static Class program

---
+++ DemoProject/StaticClassExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    static class CatWorld
+    {
+        static int NumberOfCats = 5;
+        static int TotalCatNaps = 3;
+        internal static void add()
+        {
+            NumberOfCats++;
+            Console.WriteLine("A new cat has joined.");
+            Console.WriteLine();
+        }
+        internal static void Display()
+        {
+            Console.WriteLine("Total number of cats: "+NumberOfCats);
+            Console.WriteLine(TotalCatNaps + " cats are naping.");
+            Console.WriteLine();
+        }
+    }
+
+    internal class StaticClassExample
+    {
+        static void Main()
+        {
+            CatWorld.Display();
+            CatWorld.add();
+            CatWorld.Display();
+        }
+    }
+}
